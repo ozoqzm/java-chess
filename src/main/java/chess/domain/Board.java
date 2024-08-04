56a52963f10f95a39db4a46f86736aeb5500d828
sojeong <spah7839@naver.com>
2024-08-04 14:19:30 +0900

    feat: 현재 보드 상태 출력

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -22,25 +22,18 @@ public class Board {
                 new Knight(Color.BLACK, new Position(Row.EIGHT, Column.G)),
                 new Rook(Color.BLACK, new Position(Row.EIGHT, Column.H))
         };
-
-        // Initialize black pawns
         for (int i = 0; i < 8; i++) {
             board[1][i] = new FirstBlackPawn(new Position(Row.SEVEN, Column.values()[i]));
         }
 
-        // Initialize empty spaces
         for (int i = 2; i < 6; i++) {
             for (int j = 0; j < 8; j++) {
                 board[i][j] = new Blank(new Position(Row.values()[i], Column.values()[j]));
             }
         }
-
-        // Initialize white pawns
         for (int i = 0; i < 8; i++) {
             board[6][i] = new FirstWhitePawn(new Position(Row.TWO, Column.values()[i]));
         }
-
-        // Initialize white pieces
         board[7] = new Piece[]{
                 new Rook(Color.WHITE, new Position(Row.ONE, Column.A)),
                 new Knight(Color.WHITE, new Position(Row.ONE, Column.B)),
@@ -55,6 +48,31 @@ public class Board {
 
     public Piece getPiece(int row, int column) {
         return board[row][column];
+    }
+
+    public void updatePiece(Position from, Position to, Piece piece) {
+        board[to.row().ordinal()][to.column().ordinal()] = piece.update(to);
+        board[from.row().ordinal()][from.column().ordinal()] = new Blank(from);
+    }
+
+    // 현재 보드 출력
+    public void printBoard() {
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 0; j < 8; j++) {
+                Piece piece = board[i][j];
+                if (piece.color().isEmpty()) {
+                    System.out.print('.');
+                } else {
+                    char symbol = piece.pieceType().symbol();
+                    if (piece.color().isWhite()) {
+                        System.out.print(Character.toLowerCase(symbol));
+                    } else {
+                        System.out.print(Character.toUpperCase(symbol));
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
